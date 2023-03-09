3fb49eaf6164f8a6099f363a76e866d157f474d2
Build Er <builder@macmini6.local>
2023-03-09 10:54:16 -0300

    Package version: 1.0.0-beta+20230309134517

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-1.0.0-beta+20230309100338.xcframework.zip",
-			checksum: "9b4442304c224ce8ce09dd63c5fec9bf34d313446974fc678f9d7cc2026fc0eb"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-1.0.0-beta+20230309134517.xcframework.zip",
+			checksum: "9214baeb3a8b3756b4801b97a7f9e8ade3fc8ed4ec3fce0337c88b706cc09178"
 		)
 	]
 )
