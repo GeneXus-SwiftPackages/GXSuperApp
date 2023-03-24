2639c7dc51558922b3b3bb25f1145a5253017404
genexusbot <builder@genexus.com>
2023-03-24 12:12:42 -0300

    Package version: 1.0.0-beta+20230324150340

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-1.0.0-beta+20230323132058.xcframework.zip",
-			checksum: "2367bdae9ccad908ee95be8a539c48b330f50c19772246837bf5c42c0c8f764e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-1.0.0-beta+20230324150340.xcframework.zip",
+			checksum: "6cc4dcbdcc4832667ca131dac755457dd1a00d4d62654c194fdeae3316db179b"
 		)
 	]
 )
