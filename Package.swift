48bff0321a545bea36db854283367bd9a510d75f
Build Er <builder@macmini6.local>
2023-06-01 01:52:30 -0300

    Package version: 1.0.0-beta+20230601044120

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
 		.target(name: "GXSuperAppWrapper",
 				dependencies: [
 					"GXSuperApp",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-1.0.0-beta+20230531050040.xcframework.zip",
-			checksum: "17371c5a35f78b1e4c404655dec7656908a3a27fbc7d86ae6d3767a5f15d7c79"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-1.0.0-beta+20230601044120.xcframework.zip",
+			checksum: "3f83d3c20e3f8734229fcef3e44f9dcbc7ce319e9af350d607dd30a8f5c82389"
 		)
 	]
 )
