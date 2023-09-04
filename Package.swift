ba842e366bacb1176516e40fa8978decad546254
genexusbot <builder@genexus.com>
2023-09-04 10:47:16 -0300

    Package version: 1.1.3

--- Package.swift
+++ Package.swift
@@ -10,19 +10,19 @@ let package = Package(
 			targets: ["GXSuperAppWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.2")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.3")
 	],
 	targets: [
 		.target(name: "GXSuperAppWrapper",
 				dependencies: [
 					"GXSuperApp",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .tvOS]))
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXSuperApp-1.1.2.xcframework.zip",
-			checksum: "39e61532e58da689dc827295799256b7a94d73fbf6c259c90f0f70ed7bf5b803"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXSuperApp-1.1.3.xcframework.zip",
+			checksum: "48137394610f149a9f7f6ef23c08e8016edbd9ece20ca8d2f139d4f1fe6b062f"
 		)
 	]
 )
