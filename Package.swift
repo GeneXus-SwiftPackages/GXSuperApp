4d01515749f8caba4936e7be4d5a29f39c4190d4
genexusbot <builder@genexus.com>
2024-01-18 00:37:09 -0300

    Package version: 1.4.0-rc.21

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXSuperAppWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.0-rc.10")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.4.0-rc.21")
 	],
 	targets: [
 		.target(name: "GXSuperAppWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/preview/GXSuperApp-1.4.0-rc.10.xcframework.zip",
-			checksum: "f859fb674a42b414f96503d33bf671eaf9895b99f4229422f4f3af57b2a7de18"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXSuperApp-1.4.0-rc.21.xcframework.zip",
+			checksum: "521c4ce4c51d5ec0a88e650dced0c126756438a445306e8e10993f9c5ae752c3"
 		)
 	]
 )
