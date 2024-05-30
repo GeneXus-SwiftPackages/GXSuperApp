840e80c7a8f1fa7cb4a61ed13d6100d51dcfce2b
Build Er <builder@macmini6.local>
2024-05-30 12:39:06 -0300

    Package version: 2.0.0-beta.35

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXSuperAppWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.34")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "2.0.0-beta.35")
 	],
 	targets: [
 		.target(name: "GXSuperAppWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-2.0.0-beta.34.xcframework.zip",
-			checksum: "1624db002b5a34e81e804f58234913415b7a357a99dc45e86e7821bc8675390e"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-2.0.0-beta.35.xcframework.zip",
+			checksum: "218c84d96ac7dcee7afe721a5f561811b755b21b63d15e0b2e9eb9caf76474ce"
 		)
 	]
 )
