1115313f7a1c1de15f21bc74cd8996ab4b5f5397
The iOS Team @ GeneXus <builder@genexus.com>
2025-07-16 18:58:43 -0300

    Package version: 3.2.0-beta.27

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXSuperAppWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.25"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "3.2.0-beta.25")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0-beta.27"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "3.2.0-beta.27")
 	],
 	targets: [
 		.target(name: "GXSuperAppWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-3.2.0-beta.25.xcframework.zip",
-			checksum: "97dd572c3e44227a4bac32704492ab9cb26450034cc4337f699d7dcb4c8924bb"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperApp-3.2.0-beta.27.xcframework.zip",
+			checksum: "eb11e561519c8088f8e9839ad796085832c4f53c8ebc510097b9b20fd6bf5fa5"
 		)
 	]
 )
