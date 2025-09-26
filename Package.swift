45e81d89e2e4cfed080f971ad1b0b2401bf312cc
The iOS Team @ GeneXus <builder@genexus.com>
2025-09-26 07:02:44 -0400

    Package version: 3.2.0

--- Package.swift
+++ Package.swift
@@ -1,28 +1,30 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXSuperApp",
-	platforms: [.iOS("12.0"), .tvOS("12.0")],
+	platforms: [.iOS("15.0"), .tvOS("18.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXSuperApp",
 			targets: ["GXSuperAppWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXGAM.git", exact: "3.2.0")
 	],
 	targets: [
 		.target(name: "GXSuperAppWrapper",
 				dependencies: [
 					"GXSuperApp",
-					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.tvOS, .iOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS, .visionOS])),
+					.product(name: "GXGAM", package: "GXGAM", condition: .when(platforms: [.iOS, .tvOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperApp",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXSuperApp-1.1.0.xcframework.zip",
-			checksum: "74b47e1c607ed9a3c575c43ee57b4ce8607b30be9a82f204282ec169c9a92a3a"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXSuperApp-3.2.0.xcframework.zip",
+			checksum: "7a234b850d6994669594d3ab7ee04158a5421310bcdfe61bf0f5af40ee5964c8"
 		)
 	]
 )
